4be222134a0bafecac446713b04a8db9ba602230
David Horak <info@davidhorak.com>
2023-02-07 22:23:30 -0500

    add exported types

--- goillogical.go
+++ goillogical.go
@@ -1,12 +1,43 @@
 package goillogical
 
 import (
-	. "github.com/spaceavocado/goillogical/internal"
+	intr "github.com/spaceavocado/goillogical/internal"
 	c "github.com/spaceavocado/goillogical/internal/operand/collection"
 	r "github.com/spaceavocado/goillogical/internal/operand/reference"
-	. "github.com/spaceavocado/goillogical/internal/options"
+	o "github.com/spaceavocado/goillogical/internal/options"
 	p "github.com/spaceavocado/goillogical/internal/parser"
 )
+
+type Kind = intr.Kind
+
+const And = intr.And
+const Or = intr.Or
+const Nor = intr.Nor
+const Xor = intr.Xor
+const Not = intr.Not
+
+const Eq = intr.Eq
+const Ne = intr.Ne
+const Gt = intr.Gt
+const Ge = intr.Ge
+const Lt = intr.Lt
+const Le = intr.Le
+const Nil = intr.Nil
+const Present = intr.Present
+const In = intr.In
+const Nin = intr.Nin
+const Overlap = intr.Overlap
+const Prefix = intr.Prefix
+const Suffix = intr.Suffix
+
+type OperatorMapping = intr.OperatorMapping
+type SimplifyOptions = r.SimplifyOptions
+type ReferenceSerializeOptions = r.SerializeOptions
+type CollectionSerializeOptions = struct {
+	EscapeCharacter string
+}
+type Context = intr.Context
+type Evaluable = intr.Evaluable
 
 type Goillogical interface {
 	Evaluate(any, Context) (any, error)
@@ -16,7 +47,7 @@ type Goillogical interface {
 }
 
 type illogical struct {
-	opts   Options
+	opts   o.Options
 	parser p.Parser
 }
 
@@ -25,7 +56,7 @@ func (i illogical) Evaluate(exp any, ctx Context) (any, error) {
 	if err != nil {
 		return nil, err
 	}
-	return e.Evaluate(FlattenContext(ctx))
+	return e.Evaluate(intr.FlattenContext(ctx))
 }
 
 func (i illogical) Parse(exp any) (Evaluable, error) {
@@ -52,19 +83,22 @@ func (i illogical) Simplify(exp any, ctx Context) (any, Evaluable, error) {
 
 type Option func(*illogical)
 
-func WithReferenceSerializeOptions(o r.SerializeOptions) Option {
+func WithReferenceSerializeOptions(o ReferenceSerializeOptions) Option {
 	return func(i *illogical) {
 		i.opts.Serialize.Reference = o
 	}
 }
 
-func WithCollectionSerializeOptions(o c.SerializeOptions) Option {
+func WithCollectionSerializeOptions(o CollectionSerializeOptions) Option {
 	return func(i *illogical) {
-		i.opts.Serialize.Collection = o
+		i.opts.Serialize.Collection = c.SerializeOptions{
+			EscapeCharacter:  o.EscapeCharacter,
+			EscapedOperators: map[string]bool{},
+		}
 	}
 }
 
-func WithReferenceSimplifyOptions(o r.SimplifyOptions) Option {
+func WithReferenceSimplifyOptions(o SimplifyOptions) Option {
 	return func(i *illogical) {
 		i.opts.Simplify.Reference = o
 	}
@@ -76,12 +110,8 @@ func WithOperatorMappingOptions(m OperatorMapping) Option {
 	}
 }
 
-type SimplifyOptions = r.SimplifyOptions
-type ReferenceSerializeOptions = r.SerializeOptions
-type CollectionSerializeOptions = c.SerializeOptions
-
 func New(opts ...Option) Goillogical {
-	i := &illogical{DefaultOptions(), nil}
+	i := &illogical{o.DefaultOptions(), nil}
 
 	for _, opt := range opts {
 		opt(i)
